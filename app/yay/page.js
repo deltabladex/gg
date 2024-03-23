a31c5abae8265c50ab05c357f20ff6ba912d492a
deltabladex <121876767+deltabladex@users.noreply.github.com>
2024-03-23 17:58:26 +1100

    Update page.js

--- app/yay/page.js
+++ app/yay/page.js
@@ -3,8 +3,7 @@ export default function page() {
   return (
     <div className='box'>
       <img alt='yayyy' className='gif' src='/gumball-and-darwing.gif' />
-      {'My love for you spans galaxies and beyond! <3'}
-     {'Yayyy~  Now lets Plan to be forever'}
+      {'Yayyy~  Now lets Plan to be forever'}
     </div>
   )
 
